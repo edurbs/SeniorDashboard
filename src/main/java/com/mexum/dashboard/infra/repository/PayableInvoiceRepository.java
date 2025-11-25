7a3239539ecab1955b08291db4dc36cb2313d8e6
Eduardo Soares <edurbs@gmail.com>
2025-11-25 14:28:11 -0300

    remove: unnecessary classes

--- src/main/java/com/mexum/dashboard/infra/repository/PayableInvoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mexum.dashboard.infra.repository;
-
-import com.mexum.dashboard.app.domain.PayableInvoice;
-import com.mexum.dashboard.app.domain.PayableInvoiceId;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface PayableInvoiceRepository extends JpaRepository<PayableInvoice, PayableInvoiceId>, JpaSpecificationExecutor<PayableInvoice> {
-}
